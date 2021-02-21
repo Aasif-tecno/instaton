f8db4e19bb82e969c4c4d51a967e8fb9395d68f5
Mohammed Aasif <mohammedaasif853@gmail.com>
2021-02-21 03:24:50 -0800

    precompile assets

--- config/application.rb
+++ config/application.rb
@@ -2,6 +2,7 @@ require_relative "boot"
 
 require "rails/all"
 
+
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
@@ -12,7 +13,7 @@ module Instaton
     config.load_defaults 6.1
 
     # Configuration for the application, engines, and railties goes here.
-    #
+    config.assets.initialize_on_precompile = false
     # These settings can be overridden in specific environments using the files
     # in config/environments, which are processed later.
     #
